b0c2a53eeaf11598c43d3863fe2c349016223b04
VerboseIdentity <bhuvanchandra1018@gmail.com>
2024-05-20 19:43:28 +0530

    Import Script updated

--- My Tools/Cert import/PFXcert import.ps1
+++ My Tools/Cert import/PFXcert import.ps1
@@ -1,46 +1,38 @@
 ﻿$Certpwd = Read-Host "Enter the Certificate password" -AsSecureString
 
 $Servers = Get-Content -path "..\Servers.txt"
-#C:\AHSINSTALL\TW\Misc
 
-Foreach($Server in $Servers){
+foreach($Server in $Servers) {
+    if (Test-Path "\\$Server\C$\AHSINSTALL\TW\Misc") {
+        Write-Host 'Misc folder available, proceeding ...'
+    } else {
+        New-Item -Path "\\$Server\C$\AHSINSTALL\TW" -Name Misc -ItemType Directory -ErrorAction SilentlyContinue
+    }
 
-if (Test-Path "\\$Server\C$\AHSINSTALL\TW\Misc"){
+    Remove-Item -Path "\\$Server\C$\AHSINSTALL\TW\Misc\PFX Certs" -Force -Recurse -ErrorAction SilentlyContinue
 
-Write-host 'Misc folder available, proceeding ...'
-}
-else{
+    Copy-Item -Path ".\PFX Certs" -Destination "\\$Server\C$\AHSINSTALL\TW\Misc" -Recurse -Force -ErrorAction SilentlyContinue
 
-New-Item -Path "\\$Server\C$\AHSINSTALL\TW" -Name Misc -ItemType Directory -ErrorAction SilentlyContinue
-}
+    $session = New-PSSession -ComputerName $Server
 
-Remove-Item -Path "\\$Server\C$\AHSINSTALL\TW\Misc\PFX Certs" -Force -Recurse -ErrorAction SilentlyContinue
+    $Certs = Get-ChildItem -Path "\\$Server\C$\AHSINSTALL\TW\Misc\PFX Certs"
 
-Copy-Item -Path ".\PFX Certs" -Destination "\\$Server\C$\AHSINSTALL\TW\Misc" -Recurse -Force -ErrorAction SilentlyContinue
+    foreach($Cert in $Certs) {
+        $filepath = "\\$Server\C$\AHSINSTALL\TW\Misc\PFX Certs\$Cert"
 
-$session = New-PSSession -ComputerName $Server
+        Invoke-Command -Session $session -ScriptBlock {
+            param ($filepath, $Certpwd)
+            
+            Import-PFXCertificate -CertStoreLocation Cert:\LocalMachine\My -FilePath $filepath -Password $Certpwd -Exportable
+            Import-PFXCertificate -CertStoreLocation Cert:\LocalMachine\TrustedPeople -FilePath $filepath -Password $Certpwd -Exportable
+            Import-PFXCertificate -CertStoreLocation Cert:\LocalMachine\Root -FilePath $filepath -Password $Certpwd -Exportable
 
-$Certs = Get-ChildItem -Path "\\$Server\C$\AHSINSTALL\TW\Misc\PFX Certs"
-
-$Certs = $Certs.Name
-
-foreach($Cert in $Certs){
-
-$filepath = 'C:\AHSINSTALL\TW\Misc\PFX Certs\' + $Cert
-
-Invoke-Command -Session $session -ScriptBlock {
-
-Import-PFXCertificate -CertStoreLocation Cert:\LocalMachine\My -FilePath $using:filepath -Password $using:Certpwd
- 
-Import-PFXCertificate -CertStoreLocation Cert:\LocalMachine\TrustedPeople -FilePath $using:filepath -Password $using:Certpwd
- 
-Import-PFXCertificate -CertStoreLocation Cert:\LocalMachine\Root -FilePath $using:filepath -Password $using:Certpwd
+        } -ArgumentList $filepath, $Certpwd
+    }
     
-}
-}
-Remove-PSSession $session
+    Remove-PSSession $session
 }
 
-Write-host "`nCertificates imported ...!" -ForegroundColor Green
+Write-Host "`nCertificates imported ...!" -ForegroundColor Green
 
-Powershell -noexit
+powershell -noexit
