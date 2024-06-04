b066bb957df380eb83b4aff98c5bc63fc1b86660
VerboseIdentity <bhuvanchandra1018@gmail.com>
2024-06-05 02:37:43 +0530

    Sql Customization back up file updated

--- My Tools/SQL_Customizations/SQL_Cust_Backup.ps1
+++ My Tools/SQL_Customizations/SQL_Cust_Backup.ps1
@@ -1,26 +1,46 @@
-﻿Write-Host "`nBacking up Stored_procedures.."
+﻿If (-not (Get-Module -ListAvailable -Name sqlserver)){
+Write-Host "installing SqlServer module"
+Install-Module -Name sqlserver -Force -ErrorAction Stop
+}
+else{Write-Host "SqlServer Module already present.."}
+
+Write-Host "`nBacking up Stored Procedures.."
 
 $Storage_path = ".\Command_SP"
-try{
-$Sps = Get-Content -Path ".\Command_SP\StoredProcedures.txt" -ErrorAction Stop
+$backup_location = ".\Command_SP\Backups\"
+try {
+    $SPs = Get-Content -Path ".\Command_SP\StoredProcedures.txt" -ErrorAction Stop
+} catch {
+    Write-Host "Could not get file or file contents. Please update the 'StoredProcedures.txt'" -ForegroundColor Yellow
+    Start-Sleep -Seconds 3
+    Read-Host "Hit enter to exit"
+    exit
 }
-catch{
-Write-Host "Could not get file or file contents. Please update the 'StoredProcedures.txt'" -ForegroundColor Yellow
-Start-Sleep -Seconds 3
-Read-Host "Hit enter to exit"
-exit
+
+if (Test-Path $Storage_path) {
+    Write-Host "Preliminaries ready.."
+} else {
+    New-Item -Path $Storage_path -ItemType Directory
 }
-if(Test-Path $Storage_path){Write-Host "Preliminaries ready.."}else{New-Item -Path $Storage_path -ItemType Directory}
+
+if (Test-Path $backup_location) {
+    Write-Host "Backup location ready.."
+} else {
+    New-Item -Path $backup_location -ItemType Directory
+}
 
 $DB_Server = Read-Host "Please enter the Database server name"
 $instance = Read-Host "Please enter the Database instance name"
 
+$Instance_Object = Get-SqlInstance -ServerInstance $instance
+$All_Sps = $Instance_Object.Databases["Works"].StoredProcedures
+
 foreach($SP in $SPs){
-Invoke-Command -ComputerName $DB_Server -ScriptBlock{
-$Instance = Get-sqlInstance -ServerInstance "$Using:instance"
-$Sps = $Instance.Databases["Works"].StoredProcedures
+try{
+$TargetSP = $All_Sps | Where-Object {$_.Name -eq "$SP"}
+$TargetSP.script() | Out-File "$backup_location\$($TargetSP.Name).sql"
+Write-Host "$($TargetSP.Name) Successfully backed up." -ForegroundColor Green -ErrorAction Continue
 }
-$TargetSP = $Sps | Where-Object {$_.Name -eq "$SP"}
-$TargetSP.script() | Out-File "$Storage_path\$($TargetSP.Name).sql"
+catch{Write-Host "$($TargetSP.Name) backup failed." -ForegroundColor Red}
 }
-Powershell -noexit
+powershell -noexit
